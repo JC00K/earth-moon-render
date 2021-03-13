53bdf01badf8ef161a1279e0ffc8bb310804594e
Jeremy Cook <jeremycookbiz@gmail.com>
2021-03-13 18:32:33 -0500

    Moon added, needs orbit and rotation

--- public/client.js
+++ public/client.js
@@ -5,47 +5,64 @@ import Stats from '/jsm/libs/stats.module.js';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-  45,
+  80,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.z = 2;
+camera.position.z = 2.2;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.minDistance = 1.35;
-controls.maxDistance = 2;
+controls.maxDistance = 5;
 
+// Lighting (allows all objects to be visible....a sun for ThreeJS)
 const light = new THREE.DirectionalLight(0x333333, 1);
 light.position.set(5, 3, 5).normalize();
 scene.add(light);
 scene.add(new THREE.AmbientLight(0xffffff));
 
 // Earth
-const earth = new THREE.SphereBufferGeometry(0.5, 32, 32);
+
+const earthGeometry = new THREE.SphereBufferGeometry(0.7, 32, 32);
 const earthMaterial = new THREE.MeshPhongMaterial();
-const planetEarth = new THREE.Mesh(earth, earthMaterial);
+const earth = new THREE.Mesh(earthGeometry, earthMaterial);
 earthMaterial.map = THREE.ImageUtils.loadTexture(
   '/images/Earthwithcloudsflipped.jpeg'
 );
 earthMaterial.side = THREE.BackSide;
-scene.add(planetEarth);
+scene.add(earth);
+
 // Bumps for texturing
+
 earthMaterial.bumpMap = THREE.ImageUtils.loadTexture(
   '/images/earthbump1kflipped.jpg'
 );
 earthMaterial.bumpScale = 0.05;
 
 // Specular Texture
+
 earthMaterial.specularMap = THREE.ImageUtils.loadTexture(
   '/images/earthspec1kflipped.jpg'
 );
 earthMaterial.specular = new THREE.Color('grey');
 
-const clouds = new THREE.SphereBufferGeometry(0.51, 32, 32);
+// Moon
+
+const moonGeometry = new THREE.SphereGeometry(0.2, 32, 32);
+const moonMaterial = new THREE.MeshPhongMaterial({
+  map: THREE.ImageUtils.loadTexture('/images/moonmapflipped.jpeg'),
+});
+const moon = new THREE.Mesh(moonGeometry, moonMaterial);
+moon.position.set(3, 0, 0);
+scene.add(moon);
+
+// Clouds
+
+const clouds = new THREE.SphereBufferGeometry(0.71, 32, 32);
 const cloudMaterial = new THREE.MeshPhongMaterial();
 const cloudMesh = (clouds, cloudMaterial);
 cloudMaterial.map = THREE.ImageUtils.loadTexture(
@@ -78,10 +95,10 @@ window.addEventListener(
 const stats = Stats();
 document.body.appendChild(stats.dom);
 
-var animate = function () {
+let animate = function () {
   requestAnimationFrame(animate);
-  planetEarth.rotation.x += 0;
-  planetEarth.rotation.y += -0.001;
+  earth.rotation.x += 0;
+  earth.rotation.y += -0.001;
   controls.update();
   render();
   stats.update();
